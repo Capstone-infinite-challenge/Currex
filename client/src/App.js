3f8745a9bf8aa03ef75dab3d57f60bfb2155cf76
gilmeee <127662671+gilmeee@users.noreply.github.com>
2024-11-24 16:54:45 +0900

    Feat: 채팅 구현 (미완성)

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GlobalStyle from "../src/components/style/GlobalStyle";
 import BuyMoney from "../src/components/views/BuyerPage/BuyMoney";
+import Chat from "../src/components/views/ChattingPage/Chat";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<BuyMoney />} />
+          <Route exact path="/chat" element={<Chat />} />
         </Routes>
       </BrowserRouter>
     </div>
