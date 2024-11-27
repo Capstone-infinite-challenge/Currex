1cb65f1a5df121d4383a5d9ff7beff885d753680
m2nsp <mini071663@gmail.com>
2024-11-27 15:32:54 +0900

    feat: add sellerMatch route

--- server/app.js
+++ server/app.js
@@ -1,15 +1,23 @@
 import express from 'express';
-import mongoClient from './configs/mongodb-connection.js';
 import cors from "cors";
+import dotenv from 'dotenv';
+import connectToDatabase from './configs/mongodb-connection.js';
+import Seller from './models/seller-model.js';
+
+dotenv.config();
 
 const app = express();
-const PORT = 5000; // 5000포트에서 서버실행
 
 app.use(cors());
+
 
 // req.body와 POST 요청을 해석하기 위한 설정
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//몽고디비 연결
+connectToDatabase();
+
 
 // 라우터
 
@@ -26,21 +34,28 @@ app.post('/buy', (req, res) => {
     console.log("Request body:", req.body); // 제대로 값이 들어오는지 확인용
     const sellMatchInfoRequest = {
       currency: req.body.currency,
-      KRW_minAmount: req.body.KRW_minAmount,
-      KRW_maxAmount: req.body.KRW_maxAmount,
+      minAmount: req.body.minAmount,
+      maxAmount: req.body.maxAmount,
       userLocation: req.body.userLocation,
     };
 
-    if (
-      !sellMatchInfoRequest.currency ||
-      !sellMatchInfoRequest.KRW_minAmount ||
-      !sellMatchInfoRequest.KRW_maxAmount
-    ) {
-      return res.status(400).json({
-        error: '모든 필드를 작성해야합니다',
-      });
+    if (!sellMatchInfoRequest.currency){
+        return res.status(400).json({
+            error: '금액을 입력해주세요',
+        });
+    }else if(!sellMatchInfoRequest.minAmount){
+        return res.status(400).json({
+            error: '최소금액을 입력해주세요',
+        });
+    }else if(!sellMatchInfoRequest.maxAmount){
+        return res.status(400).json({
+            error: '최대금액을 입력해주세요',
+        });
+    }else if(sellMatchInfoRequest.minAmount>sellMatchInfoRequest.maxAmount){
+        return res.status(400).json({
+            error: '최대금액은 최소금액보다 커야 합니다',
+        });
     }
-
     res.status(201).json(sellMatchInfoRequest);
   } catch (error) {
     console.error(error);
@@ -49,13 +64,26 @@ app.post('/buy', (req, res) => {
 });
 
 
-// 서버 실행
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+//판매자 매칭
+app.get('/SellerMatch', async(req, res) => {
+
+  try{
+    const { currency, minAmount, maxAmount, userLocation } = req.query;
+    const sellers = await Seller.find({
+        currency: currency,
+        amount: { $gte: Number(minAmount), $lte: Number(maxAmount)},
+      });
+
+    console.log(sellers);           //값 확인용
+    res.status(200).json(sellers);
+  }catch(error){
+    console.error(error);
+    res.status(500).json({error: '서버 오류가 발생했습니다.'});
+  }
 });
 
 
-//판매자 매칭
-//app.get('/SellerMatch', (req, res) => {
-
-//})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
