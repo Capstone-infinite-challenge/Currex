a042ea3066aa1a5ea920f2509b2e0ed8857a9c5e
m2nsp <mini071663@gmail.com>
2025-02-03 16:01:54 +0900

    refac: 리프레시토큰미들웨어

--- server/middleware/refreshTokenMiddleware.js
+++ server/middleware/refreshTokenMiddleware.js
@@ -1,32 +1,27 @@
-import jwt from 'jsonwebtoken';
-import user from '../models/user.js';
+import jwt from '../utils/jwt.js';
+import User from '../models/user.js';
 
 async function refreshTokenMiddleware(req, res){
     try{
-        const refreshToken = req.cookies.refreshToken || req.headers["authorization"]?split(" ")[1];
+        const refreshToken = req.cookies.refreshToken || req.headers["authorization"]?.split(" ")[1];
 
         if(!refreshToken){
             return res.status(401).json({message: "리프레시 토큰이 없습니다."});
         }
 
         //리프레시 토큰 검증
-        const decoded = jwt.verify(refreshToken, process.env.SECRET_KEY);
-        if(!decoded){
-            return res.status(403).json({message: "유효하지 않은 리프레시 토큰입니다."});
-        }
+        jwt.verifyToken(refreshToken);
+
 
         //DB에서 유저 조회
-        const user = await user.findOne({ refreshToken });
+        const user = await User.findOne({ refreshToken });
         if(!user || user.refreshTokenExpiresAt < new Date()){
             return res.status(403).json({message: "리프레시 토큰이 만료되었습니다. 다시 로그인하세요."});
         }
 
         //새 액세스 토큰 발급
-        const newAccessToken = jwt.sign(
-            {id: user.loginId, nickname: user.nickname},
-            process.env.SECRET_KEY,
-            {expiresIn: "1h"}
-        );
+        const newAccessToken = jwt.generateToken({id: user.loginId, nickname: user.nickname})
+
         res.status(200).json({accessToken: newAccessToken});
     }catch(error){
         console.log("새로운 액세스 토큰 발급 중 에러 발생: ", error);
