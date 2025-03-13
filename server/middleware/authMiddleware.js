6f92af49327291862a00609f1183d122c2a44d58
m2nsp <mini071663@gmail.com>
2025-03-13 14:33:13 +0900

    refactor: 디버깅 로그 삭제

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -6,11 +6,6 @@ const authenticateToken = (req, res, next) => {
   const authHeader = req.headers.authorization; // Authorization 헤더에서 토큰 가져오기
   const token = authHeader?.split(' ')[1] || req.cookies?.token; // "Bearer TOKEN"에서 TOKEN 추출
 
-  // 디버깅 로그 추가
-  console.log("[DEBUG] - Authorization Header:", authHeader);
-  console.log("[DEBUG] - Extracted Token:", token);
-  console.log("[DEBUG] - Cookies:", req.cookies);
-
   if (!token) {
     console.log("인증 실패: 토큰 없음");
     return res.status(401).json({ message: '인증 토큰이 없습니다.' });
@@ -18,7 +13,6 @@ const authenticateToken = (req, res, next) => {
 
   try {
     const decoded = verify(token, process.env.JWT_SECRET_KEY); // 토큰 검증
-    console.log("[DEBUG] - Token Decoded:", decoded);
     
     req.user = decoded; // 토큰의 페이로드를 req.user에 저장
     next(); // 다음 미들웨어 또는 컨트롤러로 진행
